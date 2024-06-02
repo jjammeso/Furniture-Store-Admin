a3aadc2326d1d6a817fa27f04d4716660d47a764
Sonam Jamtsho <sjamtsho@myseneca.ca>
2024-06-01 22:30:10 -0400

    fixed env variable

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   }
 
   function submit() {
-    fetch('http://localhost:4000/admin/login', {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/admin/login`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
